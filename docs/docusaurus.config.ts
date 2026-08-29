0cb4a5ca80471e66e39e553cb3f5ab1cc8785540
eprig <ledge.fruit4201@eagereverest.com>
2026-08-29 00:03:08 -0500

    quality of life updates

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -41,13 +41,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
-          showReadingTime: true,
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
@@ -77,9 +72,9 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'docsSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
