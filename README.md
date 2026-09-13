b2b815710310b6fc9a4217906b5609dc4b11cef0
Elliot Prigelmeier <elliotprigelmeier@gmail.com>
2026-09-13 12:53:08 -0500

    Add Documentation (#2)
    Add documentation site, make with docusaurus, added logos to assets folder, and updated readme.md and contributing.md

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
+<p align="center">
+  <img src="./assets/logo.png" width="300" alt="Logo">
+</p>
+
 # RoboRoute
+
+> [!NOTE]
+> This project was made mostly using AI, we are students trying to learn and create a project quickly, the docs were written by a human to ensure correctness
+
+The Docs are located at https://frc2207.github.io/RoboRoute
+
+Want to Contribute? Check out the `CONTRIBUTING.md` file
+
+**RoboRoute is comprised of two parts:**
+- The RoboRoute android app
+- The NTOverAOA connection layer
+
+## Quick Start
+Download the latest release from https://github.com/frc2207/RoboRoute
+
+- Extract all files from the downloaded file
+- Run the NTOverAOA application
+- Plugin your android device with USB debugging mode enabled
+- Navigate to the setup tab within the app
+- Browse for an apk file, use the file included in your extracted download, click install
+- Once the apk installation finishes, click install driver (Windows only, linux should work out of the box)
+- Navigate to the connection tab, enter your robot's ip address (`10.TE.AM.2`) or `localhost` when using simulation, and connect
+
+Done!
+Check out the [Docs](https://frc2207.github.io/RoboRoute) for more information
+
+If you have any problems, create a github issue
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,117 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'RoboRoute Docs',
+  tagline: 'AutoAlign made simple',
+  favicon: 'img/logo.svg',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://frc2207.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'frc2207', // Usually your GitHub org/user name.
+  projectName: 'RoboRoute', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        blog: {
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  plugins: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        hashed: true,
+        searchBarShortcut: false,
+        searchBarPosition: 'right',
+      },
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'RoboRoute Docs',
+      logo: {
+        alt: 'RoboRoute Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docsSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        // {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          type: 'search',
+          position: 'right',
+        },
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/FRC2207/RoboRoute',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} FRC Team 2207. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
